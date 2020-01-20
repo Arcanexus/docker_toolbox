268a84916186cbf9c21bb77d9eaeefe2e9a5e99c
Alexandre Schwartzmann <TM5855@engie.com>
2020-01-20 10:57:12 +0100

    manage all ssh certs names

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 cp -R /tmp/.ssh /root/.ssh
 chmod 700 /root/.ssh
-chmod 644 /root/.ssh/id_rsa.pub
-chmod 600 /root/.ssh/id_rsa
+chmod 600 /root/.ssh/*
+chmod 644 /root/.ssh/*.pub
 
 exec "$@"
