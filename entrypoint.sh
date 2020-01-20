aae50ceccf104a91245337b2cc9ce1822785f87c
Alexandre Schwartzmann <TM5855@engie.com>
2020-01-20 10:58:37 +0100

    allow no ssh keys

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-
-cp -R /tmp/.ssh /root/.ssh
-chmod 700 /root/.ssh
-chmod 600 /root/.ssh/*
-chmod 644 /root/.ssh/*.pub
-
+if [ -d /tmp/.ssh ]; then
+  cp -R /tmp/.ssh /root/.ssh
+  chmod 700 /root/.ssh
+  chmod 600 /root/.ssh/*
+  chmod 644 /root/.ssh/*.pub
+fi
 exec "$@"
